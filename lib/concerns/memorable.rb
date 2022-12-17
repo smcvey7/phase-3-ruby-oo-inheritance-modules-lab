9a3b625ada27fc3b950eecdde49ce39a88d8e7ee
Steven McVey <smcvey1210@gmail.com>
2022-12-17 12:14:47 -0800

    commit tree f2ee9e302eb25c48e3dfe1080ece069d87dfffc9

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,16 @@
+module Memorable
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+end
